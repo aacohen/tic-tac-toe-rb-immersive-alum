9d0a9b2ff94d528098f9cacdd06fe48e43fc3b0c
Ariela Cohen <arielaades@gmail.com>
2019-07-16 14:22:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,9 +21,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, player token)
-  player token = current_player(board)
-  board[index] = player token
+def move(board, index, player_token)
+  board[index] = player_token
 end
 
 def position_taken?(board, location)
@@ -38,6 +37,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
+  player_token = current_player(board)
   if valid_move?(board, index)
     move(board, index, player_token)
     display_board(board)
